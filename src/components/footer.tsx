3f58de293b18fa0f89aa64c4bf74758c64bed70c
nawdev <154863645+nawdev@users.noreply.github.com>
2024-01-27 03:01:01 +0700

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,9 +4,8 @@ export default function Footer() {
       <footer className="bg-slate-950 w-full flex justify-center py-8 lg:px-0 px-8 md:px-24 ">
         <p className="text-slate-500 font-light text-xs text-center">
           Designed and Coded by
-          <span className="text-slate-100"> Panji Pusaka</span> with
-          <span className="text-indigo-600"> Love</span> and
-          <span className="text-indigo-600"> Bakpia</span>
+          <span className="text-slate-100"> siputzx</span> with
+          <span className="text-indigo-600"> Love</span> and          
         </p>
       </footer>
     </>
